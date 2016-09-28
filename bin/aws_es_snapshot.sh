d3267eef2513edcf4b279388edbf175b56444c0b
Sebastian Nagel <sebastian@commoncrawl.org>
2016-09-28 15:23:21 +0200

    script to create an Elasticsearch snapshot on S3

---
+++ bin/aws_es_snapshot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set -x
+
+curl -XPUT http://localhost:9200/_snapshot/news_crawler_status  --data '{
+  "type": "s3",
+  "settings": {
+    "region": "us-east-1",
+    "bucket": "commoncrawl-news-crawler",
+    "base_path": "es-snapshots"
+  }
+}'
+
+
+TIMESTAMP=`date +%Y-%m-%d-%H-%M`
+
+curl -XPUT 'http://localhost:9200/_snapshot/news_crawler_status/'$TIMESTAMP'?wait_for_completion=true' --data '{
+    "indices": "status",
+    "ignore_unavailable": "true",
+    "include_global_state": false
+}'
+
