eafe3e215c1e0db7418697fcfa45ad005dc3ec48
Sebastian Nagel <sebastian@commoncrawl.org>
2019-12-12 14:57:38 +0100

    Elasticsearch metrics index initialization, merge changes from StormCrawler: - improve types used for numeric values for metrics mappings - use index lifecycle and delete metrics after 14 days

--- bin/ES_IndexInit.sh
+++ bin/ES_IndexInit.sh
@@ -58,18 +58,34 @@ curl $ESCREDENTIALS -s -XDELETE "$ESHOST/metrics*/" >  /dev/null
 echo ""
 echo "Deleted metrics index"
 
+curl $ESCREDENTIALS -s -XPUT $ESHOST/_ilm/policy/14d-deletion_policy -H 'Content-Type:application/json' -d '
+{
+    "policy": {
+        "phases": {
+            "delete": {
+                "min_age": "14d",
+                "actions": {
+                    "delete": {}
+                }
+            }
+        }
+    }
+}
+'
+
 echo "Creating metrics index with mapping"
 
 # http://localhost:9200/metrics/_mapping/status?pretty
 curl $ESCREDENTIALS -s -XPOST $ESHOST/_template/storm-metrics-template -H 'Content-Type: application/json' -d '
 {
-  "template": "metrics*",
+  "index_patterns": "metrics*",
   "settings": {
     "index": {
       "number_of_shards": 1,
       "refresh_interval": "30s"
     },
-    "number_of_replicas" : 0
+    "number_of_replicas": 0,
+    "lifecycle.name": "14d-deletion_policy"
   },
   "mappings": {
       "_source":         { "enabled": true },
@@ -84,13 +100,13 @@ curl $ESCREDENTIALS -s -XPOST $ESHOST/_template/storm-metrics-template -H 'Conte
             "type": "keyword"
           },
           "srcTaskId": {
-            "type": "long"
+            "type": "short"
           },
           "srcWorkerHost": {
             "type": "keyword"
           },
           "srcWorkerPort": {
-            "type": "long"
+            "type": "integer"
           },
           "timestamp": {
             "type": "date",
